58a03445aa85041682fe47b89f44792d337dd865
Arittro7 <arittrowpdev@gmail.com>
2026-01-28 13:19:42 +0600

    frontend

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,7 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { defineConfig, env } from '@prisma/config';
+import "dotenv/config";
 
 export default defineConfig({
   datasource: {
-    url: 'postgresql://neondb_owner:npg_MvIm4qQ0FZbW@ep-royal-dew-ah7v7ago-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+    url: env('DATABASE_URL'),
   },
 });
